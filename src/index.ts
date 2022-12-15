03f36d9e17dc714e955d99646b6647dcfda8406b
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2022-12-15 13:11:57 +0100

    better typing

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import vhtml from 'vhtml'
 export const html = htm.bind(vhtml)
 
 
-export const template = (strings, ...values) => {
+export const template = (strings: TemplateStringsArray, ...values: unknown[]) => {
   const tmpl$ = document.createElement('template')
   tmpl$.innerHTML = html(strings, ...values) as string
 
